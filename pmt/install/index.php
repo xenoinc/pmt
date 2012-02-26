479cf6b856e3bea3acbbe9c373805a06ceeb0fe3
Damian Suess <dsuess@xenoinc.org>
2012-02-26 11:34:05 -0500

    installer 75% - style is still being messed up by browser & apache 2.2 for some reason

--- pmt/install/index.php
+++ pmt/install/index.php
@@ -6,16 +6,35 @@
  * Author:       suessdam
  * Document:     index
  * Created Date: Jan 30, 2012
- * 
- * Description:
- *  Setup database script
  *
+ * Description:
+ *  xiPMT system installation
+ *
+ * To Do:
+ *  [ ] Step 2.a - Place into a function so we can resuse it in 2.b
+ *  [ ] Step 2.a - Use disappearing suggestive text
  * Change Log:
- * 
+ *  2012-0225 * 50% there (djs)
  */
 
 require_once "installer.php";
 require_once "../lib/common/pmt.db.php";
+
+$defVal = array();
+$defVal["server"] = "localhost";
+$defVal["user"]   = "testuser";
+$defVal["pass"]   = "testpass";
+$defVal["dbname"] = "PMT_DATA";
+$defVal["prefix"] = "XIPMT_";
+
+/*
+ * Auto disappear text
+  <input name="myvalue" type="text" onFocus="if(this.value=='enter value')this.value='';" value="enter value">
+  <input type="text" name="inputBox" placeholder="enter your text here">
+  <input type="text" name="theName" value="DefaultValue" style="color:#BBB;"
+         onblur="if(this.value == ''){ this.value = 'DefaultValue'; this.style.color = '#BBB';}"
+         onfocus="if(this.value == 'DefaultValue'){ this.value = ''; this.style.color = '#000';}" />
+*/
 
 /**
  * Get Confiuration Data
@@ -26,13 +45,14 @@ require_once "../lib/common/pmt.db.php";
 function getData($cfgItem, $default)
 {
   global $conf;
-  
+
   if (isset($conf["db"][$cfgItem]))
     return $conf["db"][$cfgItem];
   else
     return $default;
 }
 
+// Get the step in the setup process
 $step = (isset($_POST['step']) ? $_POST['step'] : 1);
 
 switch ($step)
@@ -45,7 +65,7 @@ switch ($step)
 
     if (IsInstalled())
     {
-      htmlMessage("xiPMT is already Installed");  
+      htmlMessage("xiPMT is already Installed");
     }
     elseif(file_exists("../lib/config.php"))
     {
@@ -66,25 +86,25 @@ switch ($step)
       </form>
       <?php
 
-    }  
+    }
     break;
-  
+
   /**
-   * 
+   * Database Setup and Testing
    */
   case 2:
     if (IsInstalled())
       break;
-    
-    
+
+
     if(isset($_POST["db"]) == false)
     {
       CreateHeader("Step 2.a");
-      
+
       // There is no db connetion info. Let get some
       if(file_exists("../lib/config.php"))
         require_once "../lib/config.php";
-      
+
       ?>
       <form action="index.php" method="post">
         <input name="step" type="hidden" value="<?php print($step); ?>" />
@@ -92,23 +112,23 @@ switch ($step)
         <table class="inputForm">
           <tr>
             <td class="label">Server:</td>
-            <td><input type="text" name="db[server]" autocomplete="off" value="<?php print(getData("server")); ?>" /> </td>
+            <td><input type="text" name="db[server]" autocomplete="off" value="<?php print(getData("server", $defVal["server"])); ?>" /> </td>
           </tr>
           <tr>
             <td class="label">Username:</td>
-            <td><input type="text" name="db[user]" autocomplete="off" value="<?php print(getData("user")); ?>" /> </td>
+            <td><input type="text" name="db[user]" autocomplete="off" value="<?php print(getData("user", $defVal["user"])); ?>" /> </td>
           </tr>
           <tr>
             <td class="label">Password:</td>
-            <td><input type="text" name="db[pass]" autocomplete="off" value="<?php print(getData("pass")); ?>" /> </td>
+            <td><input type="text" name="db[pass]" autocomplete="off" value="<?php print(getData("pass", $defVal["pass"])); ?>" /> </td>
           </tr>
           <tr>
             <td class="label">Database Name:</td>
-            <td><input type="text" name="db[dbname]" autocomplete="off" value="<?php print(getData("dbname")); ?>" /> </td>
+            <td><input type="text" name="db[dbname]" autocomplete="off" value="<?php print(getData("dbname", $defVal["dbname"])); ?>" /> </td>
           </tr>
           <tr>
             <td class="label">Database Table Prefix:</td>
-            <td><input type="text" name="db[prefix]" autocomplete="off" value="<?php print(getData("prefix")); ?>" /> </td>
+            <td><input type="text" name="db[prefix]" autocomplete="off" value="<?php print(getData("prefix", $defVal["prefix"])); ?>" /> </td>
           </tr>
         </table>
         <div id="actions">
@@ -116,13 +136,13 @@ switch ($step)
         </div>
       </form>
       <?php
-      
+
     }
     else
     {
       /* Connection Test and display fail/success */
       CreateHeader("Step 2.b");
-      
+
       // We have DB Connection Information
       $fail = false;
       $conn = mysql_connect($_POST["db"]["server"],
@@ -130,7 +150,7 @@ switch ($step)
                             $_POST["db"]["pass"]);
       if(!$conn) $fail = true;
       if(!mysql_select_db($_POST["db"]["dbname"], $conn)) $fail = true;
-      
+
       /* DB connection failure */
       if($fail)
       {
@@ -140,15 +160,17 @@ switch ($step)
           <p>Make sure the connection data was entered correctly and
           that your database has been created.</p>
         </div>
-        
+
         <form action="index.php" method="post">
           <input type="hidden" name="step" value="<?php print($step); ?>" />
           <div id="actions"><input type="submit" value="Back" /></div>
         </form>
-        
+
         <?php
-        //GenStep2Database();
-        
+
+        // Include the DB Input screen again so we don't have to click, Back.
+        //  GenStep2Database();
+
       }
       else
       {
@@ -163,13 +185,132 @@ switch ($step)
       }
     }
     break;
-  
-  
   /**
-   *Something didn't happen right 
+   * System Administratior Setup
+   */
+  case 3:
+    if (IsInstalled())
+      break;
+
+    $arrErr = array();
+    $tmp = " cannot be blank.";
+
+    if(isset($_POST["settings"]) && empty($_POST["settings"]["title"])) $arrErr["title"] = "Title$tmp";
+    if(isset($_POST["settings"]) && empty($_POST["admin"]["username"])) $arrErr["username"] = "Username$tmp";
+    if(isset($_POST["settings"]) && empty($_POST["admin"]["password"])) $arrErr["password"] = "Password$tmp";
+    if(isset($_POST["settings"]) && empty($_POST["admin"]["email"]))    $arrErr["email"] = "Email$tmp";
+
+    if(!isset($_POST["settings"]) || count($arrErr))
+    {
+      CreateHeader("Step 3.a - Admin Settings");
+
+      if (isset($_POST[""]) && count($arrErr))
+      {
+        ?>
+        <div class="message error">
+          <?php print(implode("<br />", $arrErr)); ?>
+        </div>
+        <?php
+      }
+
+      ?>
+      <form action="index.php" method="post">
+        <input type="hidden" name="step" value="<?php ?>" />
+        <input type="hidden" name="db" value="<?php ?>" />
+
+        <h2>Administration Configuration</h2>
+
+        <h3>xiPMT Settings</h3>
+        <table class="inputForm">
+          <tbody>
+            <tr><td class="label">xiPMT Title</td><td><input type="text" name="settings[title]" autocomplete="off" value="" /></td></tr>
+            <tr>
+              <td class="label">Clean URI</td>
+              <td>
+                <input type="radio" id="rdoCleanUri1" name="settings[seo_url]" value="1" checked="checked" /><label for="rdoCleanUri">Yes</label>
+                <input type="radio" id="rdoCleanUri2" name="settings[seo_url]" value="0" /><label for="rdoCleanUri2">No</label>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h3>Administration Settings</h3>
+        <tabel class="inputForm">
+          <tbody>
+            <tr><td class="label">Username:</td><td><input type="text" name="admin[username]" /></td></tr>
+            <tr><td class="label">Password:</td><td><input type="text" name="admin[password]" /></td></tr>
+            <tr><td class="label">Email:</td>   <td><input type="text" name="admin[email]" />   </td></tr>
+          </tbody>
+        </tabel>
+        <div id="actions">
+          <input type="submit" value="Next" />
+        </div>
+      </form>
+      <?php
+
+    }
+    elseif(isset($_POST["settings"]) && !count(@$arrErr))
+    {
+
+      CreateHeader("Step 3.b - Verify Settings");
+
+      ?>
+      <form action="index.php" method="post">
+        <input type="hidden" name="step"      value="<?php print(step+1); ?>" />
+        <input type="hidden" name="db"        value="<?php print(step+1); ?>" />
+        <input type="hidden" name="settings"  value="<?php print(step+1); ?>" />
+        <input type="hidden" name="admin"     value="<?php print(step+1); ?>" />
+
+        <h2>Administration Configuration</h2>
+
+        <h3>xiPMT Settings</h3>
+        <table class="inputForms">
+          <tr>
+            <td class="label">xiPMT Title</td>
+            <td><?php print($_POST["settings"]["title"]); ?></td>
+          </tr>
+          <tr>
+            <td class="label">Use Clean URI</td>
+            <td><?php print(@$_POST["settings"]["seo_url"] ? "Yes" : "No" ); ?></td>
+          </tr>
+        </table>
+
+        <h3>Administration Settings</h3>
+        <table class="inputForm">
+          <tr>
+            <td class="label"></td>
+            <td></td>
+          </tr>
+        </table>
+
+        <div id="actions">
+          <input type="submit" value="Install" />
+        </div>
+      </form>
+
+      <?php
+    }
+    else
+    {
+      CreateHeader("Step 3.a - Admin Settings (err)");
+      ?>
+      <form action="index.php" method="post">
+        <div>
+          something fucked up
+        </div>
+      </form>
+      <?php
+    }
+
+    break;
+
+
+
+  /**
+   *Something didn't happen right
    */
   default:
-    
+
     /* Connection Test and display fail/success */
     CreateHeader("Step UNKNOWN!");
     ?>
