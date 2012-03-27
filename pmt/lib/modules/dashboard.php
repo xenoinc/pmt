de58e2719a305c2b749387dde6a11e699aea01fc
Damian Suess <dsuess@xenoinc.org>
2012-03-26 21:14:50 -0400

    still cleaning up the main skin. Also updated documentation

--- pmt/lib/modules/dashboard.php
+++ pmt/lib/modules/dashboard.php
@@ -34,14 +34,14 @@ class dashboard implements pmtModule
      */
 
     // Get title from database setting
-    $this->_title = "Dashboard" . "- " . "[PMT]";    // "Xeno Tracking System"
+    $this->_title = "Dashboard " . " - " . "[PMT]";    // "Xeno Tracking System"
     $this->_toolbar = $this->GenerateToolbar();
     $this->_minileft = $this->GenerateMiniLeft(); // "&nbsp; (test-left)";
     $this->_miniright = $this->GenerateMiniRight();  // "&nbsp; (test-right)";
   }
 
-  public function Title() { return $this->_title; }
-  public function Toolbar() { return $this->_toolbar; }
+  public function Title() { return $this->_title; }             /* Title of the generated page */
+  public function Toolbar() { return $this->_toolbar; }         /* Toolbar - HTML generated toolbar according to location */
   public function MiniBarLeft() { return $this->_minileft; }
   public function MiniBarRight() { return $this->_miniright; }
 
@@ -75,49 +75,80 @@ class dashboard implements pmtModule
 
     /* Step 1 - Get user permissions */
 
+
     /* Step 2 - Generate Toolbar */
-    $t = "        ";
 
-    // $tbar = array("main", "projects");
-    $tbar = array();
+    // List of all the available modules
+    // ** This should be pulled from DB depending on user/group
+    //    permissions & settings!!
+    $arrAvailMods = array(
+          // Module         Display
+    //    "dashboard"   => "Dashboard",
+          ""            => "Dashboard",
+          "project"     => "Projects",
+          "ticket"      => "Tickets",     /* "ticket" => array ("Tickets", "+"), */
+          "bugs"        => "Bugs",
+          "tasks"       => "Tasks",
+          "product"     => "Products",
+          "customer"    => "Customers",
+          "user"        => "Users",
+          "admin"       => "Admin"
+          );
+
+    $tab = "        ";
+    $ret = $tab . "<ul>". PHP_EOL;
+    $ndxCount = 0;
+    //print (count($a));
+    foreach($arrAvailMods as $key => $value)
+    { //print ("key: $key, Obj: $value <br />");
+
+      $ndxCount++;
+      if ($ndxCount == 1)
+        $cls = ' class="first active"';
+      elseif($ndxCount == count($arrAvailMods))
+        $cls = ' class="last"';
+      else
+        $cls = '';
+
+      $ret .= $tab .
+              "  <li" . $cls. ">" .
+              $this->makeLink($key, $value) .
+              "</li>" . PHP_EOL;
+
+    }
+    $ret .= $tab . "</ul>". PHP_EOL;
+    //pmtDebug("disp: " . $ret);
+
+    return $ret;
+
+    /**
+     * OLD METHOD
+     *
+    $arrLinks = array();
     $tmod = array();
-
-    array_push($tmod, "dashboard"); array_push($tbar, "Dashboard");
-    array_push($tmod, "project");   array_push($tbar, "Projects");
-    array_push($tmod, "ticket");    array_push($tbar, "Tickets" . "&nbsp;&nbsp;" . "+");
-    array_push($tmod, "product");   array_push($tbar, "Products");
-    array_push($tmod, "customer");  array_push($tbar, "Customers");
-    array_push($tmod, "admin");     array_push($tbar, "Admin");
-    //array_push($tmod, "&nbsp;");     array_push($tbar, "&nbsp;"); // add right border
-
-    //$tmod = array("dashboard", "project", "ticket", "product", "customer", "admin");
-    //$tbar = array("dashboard", "project", "ticket", "product", "customer", "admin");
-
-
-    $ret = $t . "<ul>". PHP_EOL;
+    array_push($tmod, "dashboard"); array_push($arrLinks, '<a href="/">Dashboard</a>');
+    array_push($tmod, "project");   array_push($arrLinks, '<a href="project">Projects</a>');
+    array_push($tmod, "ticket");    array_push($arrLinks, 'Tickets' . '&nbsp;&nbsp;' . '+');
+    array_push($tmod, "product");   array_push($arrLinks, 'Products');
+    array_push($tmod, "customer");  array_push($arrLinks, 'Customers');
+    array_push($tmod, "admin");     array_push($arrLinks, 'Admin');
 
     for ($ndx = 0; $ndx < count($tmod); $ndx++)
     {
       // pmtDebug($ndx);
-
-
-
       // Draw toolbar and set the active item
       if ($ndx == 0){
         $cls = ' class="first active"';
-      }elseif ($ndx == count($tbar)-1){
+      }elseif ($ndx == count($arrLinks)-1){
         $cls = ' class="last"';
       }else{
         $cls = '';
       }
-      $ret .= $t . "  <li" . $cls. ">" . $tbar[$ndx] . "</li>" . PHP_EOL;
-
+      $ret .= $tab . "  <li" . $cls. ">" . $arrLinks[$ndx] . "</li>" . PHP_EOL;
     }
+    */
 
-    $ret .= $t . "</ul>". PHP_EOL;
-    //pmtDebug("disp: " . $ret);
 
-    return $ret;
   }
 
   private function GenerateMiniLeft()
@@ -137,6 +168,18 @@ class dashboard implements pmtModule
 
     return $code;
   }
+
+  /**
+   * Make link
+   * @param type $module
+   * @param type $text
+   * @return type
+   */
+  private function makeLink($module, $text)
+  {
+    return '<a href="'. $module.'">'.$text.'</a>';
+  }
+
 
 }
 
--- pmt/lib/modules/project.php
+++ pmt/lib/modules/project.php
@@ -17,30 +17,113 @@
 require ("pmtModule.php");
 class project implements pmtModule {
 
+  private $_title;      // Title of the screen
+  private $_toolbar;    // HTML generated toolbar according to location
+  private $_minileft;   // mini toolbar (left)
+  private $_miniright;  // mini toolbar (right)
+
   function __construct()
   {
-    return "hell world";
+    $this->_title = "Project " . " - " . "[PMT]";    // "Xeno Tracking System"
+    $this->_toolbar = $this->GenerateToolbar();
+    $this->_minileft = "";
+    $this->_miniright = "";
   }
+
+  public function Title() { return $this->_title; }             /* Title of the generated page */
+  public function Toolbar() { return $this->_toolbar; }         /* Toolbar - HTML generated toolbar according to location */
+  public function MiniBarLeft() { return $this->_minileft; }
+  public function MiniBarRight() { return $this->_miniright; }
 
   public function PageData()
   {
-    return "Project module data";
+    /**
+     * Depending on usr permissions settings, list all projects available to
+     * the user logged in.
+     */
+    $sample =  "<h1>Welcome to <b><i>xeno</i>PMT</b></h1>";
+    $sample .= "<p>This system is still under heavy development and is not ";
+    $sample .= "ready for live action use by any means. Soon enough you will ";
+    $sample .= "get to see what the future holds.  As the project develops the ";
+    $sample .= "user and engineering documentation will mature along with it.</p>";
+    $sample .= "<p>Sit tight and enjoy the ride!</p>";
+    $sample .= "<p>&nbsp;</p>";
+    $sample .= "<p>- Xeno Innovations, Inc. -</p>";
+
+    return $sample;
   }
 
-  /**
-   * Title of the generated page
-   */
-  public function Title()
+
+
+  /* **************************** */
+  /* **************************** */
+
+
+  private function GenerateToolbar()
   {
-    return "Project";
+    /* Steps:
+    * 1) Get user profile permissions to see what
+    *    items we can draw on the screen.
+    * 2) Generate toolbar
+    */
+
+    /* Step 1 - Get user permissions */
+
+
+    /* Step 2 - Generate Toolbar */
+
+    // List of all the available modules
+    // ** This should be pulled from DB depending on user/group
+    //    permissions & settings!!
+    $arrAvailMods = array(
+          // Module       Display
+          "dashboard" => "Dashboard",
+          "project"   => "Projects",
+          "ticket"    => "Tickets",     /* "ticket" => array ("Tickets", "+"), */
+          "bugs"      => "Bugs",
+          "tasks"     => "Tasks",
+          "product"   => "Products",
+          "customer"  => "Customers",
+          "user"      => "Users",
+          "admin"     => "Admin"
+          );
+
+    $tab = "        ";
+    $ret = $tab . "<ul>". PHP_EOL;
+    $ndxCount = 0;
+    //print (count($a));
+    foreach($arrAvailMods as $key => $value)
+    { //print ("key: $key, Obj: $value <br />");
+
+      $ndxCount++;
+      if ($ndxCount == 1)
+        $cls = ' class="first active"';
+      elseif($ndxCount == count($arrAvailMods))
+        $cls = ' class="last"';
+      else
+        $cls = '';
+
+      $ret .= $tab .
+              "  <li" . $cls. ">" .
+              $this->makeLink($key, $value) .
+              "</li>" . PHP_EOL;
+
+    }
+    $ret .= $tab . "</ul>". PHP_EOL;
+    //pmtDebug("disp: " . $ret);
+    return $ret;
   }
 
+
   /**
-   * Toolbar - HTML generated toolbar according to location
+   * Make link
+   * @param type $module
+   * @param type $text
+   * @return type
    */
-  public function Toolbar()
+  private function makeLink($module, $text)
   {
-    return "";
+    return '<a href="'. $module.'">'.$text.'</a>';
   }
 
 }
