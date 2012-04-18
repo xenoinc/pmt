be04a91d4d6097ca13c1044ab16b0f079a5abc16
Damian Suess <dsuess@xenoinc.org>
2012-04-18 18:37:09 -0400

    added kb module & todo list item

---
+++ pmt/lib/modules/kb.php
@@ -0,0 +1,32 @@
+<?php
+
+/* * **********************************************************
+ * Copyright 2012 (C) Xeno Innovations, Inc.
+ * ALL RIGHTS RESERVED
+ * Author:       Damian Suess
+ * Document:     kb.php - Knowledge Base
+ * Created Date: Apr 18, 2012
+ *
+ * Description:
+ *  Knowledge base class. Syntax uses WikiSyntax as its engine
+ *
+ * URL Examples:
+ *    http://pmt/kb/<id>
+ *    http://pmt/kb/?cmd=new
+ *    http://pmt/kb/?cmd=edit
+ *    http://pmt/kb/?cmd=remove
+ *
+ *    POST:
+ *      Vote:#   - Rate article 0-5
+ *
+ * Change Log:
+ *
+ */
+
+require ("pmtModule.php");
+class project implements pmtModule
+{
+  //put your code here
+}
+
+?>
