2d8d4cfd25f8e226c4f2d3e03b780ab49295caed
Damian Suess <dsuess@xenoinc.org>
2012-04-18 22:34:27 -0400

    .

---
+++ pmt/lib/modules/ext/project.rss.php
@@ -0,0 +1,22 @@
+<?php
+
+/* * **********************************************************
+ * Copyright 2012 (C) Xeno Innovations, Inc.
+ * ALL RIGHTS RESERVED
+ * Author:       Damian Suess
+ * Document:     project.rss
+ * Created Date: Apr 18, 2012
+ *
+ * Description:
+ *  Project RSS Feeds. Each project should be allowed its own
+ *  RSS feed.. that is if you want it to. To edit this feature
+ *  it is bascially like a diary entry and it just gets added on.
+ *
+ *  Probably should use the table PMT_MODULE_SETTINGS to cap off
+ *  the Max_Days or Max_Entries as well as Enabling it the feature (?).
+ *
+ * Change Log:
+ *
+ */
+?>
+
