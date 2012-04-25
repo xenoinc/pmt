58d06b5e640f7e1dd39b3526be0c99ee0744b9d9
Damian Suess <dsuess@xenoinc.org>
2012-04-25 19:21:22 -0400

    nightly

--- pmt/lib/modules/ext/project.ext.php
+++ pmt/lib/modules/ext/project.ext.php
@@ -22,18 +22,105 @@
 
 class ProjExt
 {
+  //private static function IsDate($string)
+  #accepts a date, and a date format (not a regular expression)
+  //private static function isDate($d, $f='%d-%m-%Y %H:%M')
+  private static function IsDate($d, $f='%Y-%m-%d')
+  {
+    $dateFmtRE = array(
+      '/\//' => '\/',
+      '/%g|%G|%y|%Y/' => '(19\d\d|20\d\d)',
+      '/%m/' => '(0?[1-9]|1[012])',
+      '/%d|%e/' => '(0?[1-9]|[12][0-9]|3[01])',
+      //'/%H|%I|%l/' => '([0-1]?\d|2[0-3])',
+      //'/%M/' => '([0-5]\d)'
+      );
+    if (empty($d))  return true;
+
+    #convert Unix timestamp to a std format (must not include regular expressions)
+    if (preg_match('!\d{5,}!', $d))
+      $d = strftime($f, $d);
+
+    return (
+      #does %d match the regular expression version of $f? if it does m/d/y are in $x
+      preg_match('!^' .preg_replace(array_keys($dateFmtRE), array_values($dateFmtRE),$f) .'$!', $d, $x)
+      && (checkdate($x[2], $x[1], $x[3]) || checkdate($x[1], $x[2], $x[3]) || checkdate($x[3], $x[1], $x[2]))
+      ?true :false);
+  }
+
+  private static function IsDate2($string)
+  {
+
+    $t = strtotime($string);
+    $m = date('m',$t);
+    $d = date('d',$t);
+    $y = date('Y',$t);
+    return checkdate ($m, $d, $y);
+  }
+
   static function Page_ProjectNew()
   {
 
-    if (isset($_POST["project"]) && $_POST["project"] == "" )
+    $styProjName="";    $prevProjName="";
+    $styCreatedDTTM=""; $prevCreatedDTTM="";
+    $styDescription=""; $prevDescription="";
+
+
+
+    if (isset($_POST["project"]) && $_POST["project"] == "newproj" )
     {
-      //print("Blank Submittion");
+      $errCnt = 0;
+      // Submittion attempt for a New Project
+
+
+      // Project Name (NoSpaces)
+      if(empty($_POST["txtProjName"]))
+      {
+        $errCnt++;
+        $styProjName = "background-color:#FF0000;";
+        $prevProjName = $_POST["txtProjName"];
+      }
+      else
+        $prevProjName = $_POST["txtProjName"];
+
+      // Created Date Time
+      if( empty($_POST["txtCreatedDTTM"]) )
+      {
+        $errCnt++;
+        $styCreatedDTTM = "background-color:#FF0000;";
+        $prevCreatedDTTM = $_POST["txtCreatedDTTM"];
+      }
+      //elseif (self::IsDate2($_POST["txtCreatedDTTM"], "%Y-%m-%d") == false )
+      //elseif (self::IsDate2($_POST["txtCreatedDTTM"]) == false )
+      if (!empty($_POST["txtCreatedDTTM"]) &&
+          (!ereg("^[0-9]{4}-[0-9]{2}-[0-9]{2}", $_POST["txtCreatedDTTM"]))
+          //(!ereg("^[0-9]{4}-[0-9]{2}-[0-9]{2}", $_POST["txtCreatedDTTM"]))
+         )
+      {
+        $errCnt++;
+        $styCreatedDTTM = "background-color:#FF0000;";
+        $prevCreatedDTTM = $_POST["txtCreatedDTTM"];
+      }
+      else
+        $prevCreatedDTTM = $_POST["txtCreatedDTTM"];
+
+      // Description
+      if(empty($_POST["txtDescription"]))
+      {
+        $errCnt++;
+        $styDescription = "background-color:#FF0000;";
+        $prevDescription = $_POST["txtDescription"];
+      }
+      else
+        $prevDescription = $_POST["txtDescription"];
+
     }
-    elseif (isset($_POST["project"]) && $_POST["project"] == "newproj" )
+    elseif (isset($_POST["project"]) && $_POST["project"] == "" )
     {
 
+      // Invalid post sent
       // Check input if it is valid. If not, redisplay $html below.
-      print ("Submit attempted");
+
 
     }
     // heredoc
@@ -42,7 +129,7 @@ class ProjExt
         <!-- <div class="tablethin"> -->
         <div>
           <form action="/p?cmd=new" method="post">
-            <input type="hidden" name="project" value="newproj" />
+            <div id="mute_w3c"><input type="hidden" name="project" value="newproj" /></div>
             <table width="800" cellspacing="0" class="tablethin">
               <tr><td class="tblheader first" colspan="2">Project Name</td></tr>
               <tr>
@@ -50,13 +137,13 @@ class ProjExt
                   Name of your project<br />
                   <i>(Only Alpha-Numeric, no spaces, no slashes, no BS!)</i>
                 </td>
-                <td style="text-align: right;"><input type="text" name="txtProjName" value="" /></td>
+                <td style="text-align: right;"><input type="text" name="txtProjName" value="{$prevProjName}" style="{$styProjName}" /></td>
               </tr>
 
               <tr><td class="tblheader" colspan="2">Created Date</td></tr>
               <tr>
                 <td>When was the project created <i>(YYYY-MM-DD)</i></td>
-                <td style="text-align: right;"><input type="text" name="txtCreatedDTTM" value="YYY-MM-DD" /></td>
+                <td style="text-align: right;"><input type="text" name="txtCreatedDTTM" value="{$prevCreatedDTTM}" style="{$styCreatedDTTM}" /></td>
               </tr>
 
 
@@ -93,8 +180,8 @@ class ProjExt
               <tr><td class="tblheader" colspan="2">Description</td></tr>
               <tr>
                 <td colspan="2">
-                  <textarea name="txtDescription" rows="7" cols="0"
-                  style="width:99%;"> </textarea>
+                  <textarea name="txtDescription" rows="7" cols="0" style="{$styDescription}"
+                  style="width:99%;">{$prevDescription}</textarea>
                 </td>
               </tr>
 
