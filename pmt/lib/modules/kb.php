34ec14698199fe76034051e08b88120907379d48
Damian Suess <dsuess@xenoinc.org>
2012-06-19 00:22:55 -0400

    nightly. need to get kb up and moving. This will be the basis for wikitext

--- pmt/lib/modules/kb.php
+++ pmt/lib/modules/kb.php
@@ -13,20 +13,44 @@
  * URL Examples:
  *    http://pmt/kb/<id>
  *    http://pmt/kb/?cmd=new
- *    http://pmt/kb/?cmd=edit
- *    http://pmt/kb/?cmd=remove
+ *    http://pmt/kb/<id>?cmd=edit
+ *    http://pmt/kb/<id>?cmd=remove
  *
  *    POST:
  *      Vote:#   - Rate article 0-5
  *
  * Change Log:
+ *  2012-0618 + Ground breaking
  *
  */
 
 require ("pmtModule.php");
-class project implements pmtModule
+class kb implements pmtModule
 {
-  //put your code here
+  //function __construct($uriPath = "")
+  function __construct()
+  {
+    global $uri;
+
+    if (count($uri->seg)>1)
+          $this->_PROJ_Name = self::MODULE."/".$uri->seg[1];
+    else  $this->_PROJ_Name = self::MODULE;
+
+    // Get the segments and the mode to be used
+    $this->ParseData();
+
+    $this->_title = "Knowledge Base " . " - " . "[xenoPMT]";    // "Xeno Tracking System"
+    $this->_toolbar = ""; //$this->GenerateToolbar();
+    $this->_minileft = "";
+    $this->_miniright = $this->GenerateMiniRight();  // "&nbsp; (test-right)";
+    $this->_pagedata = $this->GeneratePage();
+  }
+
+  public function Title() { return $this->_title; }             /* Title of the generated page */
+  public function Toolbar() { return $this->_toolbar; }         /* Toolbar - HTML generated toolbar according to location */
+  public function MiniBarLeft() { return $this->_minileft; }
+  public function MiniBarRight() { return $this->_miniright; }
+  public function PageData() { return $this->_pagedata; }
 }
 
 ?>
